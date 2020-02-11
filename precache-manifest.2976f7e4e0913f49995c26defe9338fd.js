4e1824f957d7fbd5ddc395e510e72b2b3e535fad
Maria <murikmarusia@gmail.com>
2020-02-11 23:44:48 +0200

    Updates

---
+++ precache-manifest.2976f7e4e0913f49995c26defe9338fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dc104ce54d0bc1a224679e9f292219",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "671757cf77c5959b7cd4",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-demo/static/js/2.b1939bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da544f7fb447a2f0fff",
+    "url": "/cra-demo/static/js/main.749fe195.chunk.js"
+  },
+  {
+    "revision": "107a7a11fab40b66012c",
+    "url": "/cra-demo/static/js/runtime-main.b7e94f1e.js"
+  }
+]);
